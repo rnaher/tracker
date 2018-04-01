e75e92c9fe63c2b8347bd52d3516bb4d1a1eae5e
rnaher <rupali1015@gmail.com>
2018-04-01 22:36:28 +0530

    changes for seller id in registration

--- PackageTrackingApp/app/src/main/java/team4/packagetrackingapp/User.java
+++ PackageTrackingApp/app/src/main/java/team4/packagetrackingapp/User.java
@@ -13,16 +13,18 @@ public class User {
     private String username;
     private String password;
     private String user_type;
+    private String seller_id;
 
     User(String name, String contact_no, String email_id,
          String username, String password,
-         String user_type) {
+         String user_type,String seller_id) {
         this.name = name;
         this.contact_no = contact_no;
         this.email_id = email_id;
         this.username = username;
         this.password = password;
         this.user_type = user_type;
+        this.seller_id =seller_id;
     }
 
     public void show() {
