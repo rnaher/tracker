edd6677f6b1d26b53e0cf65f0a0dee15e99caa12
octopada <gogollian@gmail.com>
2018-04-05 02:05:57 +0530

    track package bug fixed

--- PackageTrackingApp/app/src/main/java/team4/packagetrackingapp/TrackPackage.java
+++ PackageTrackingApp/app/src/main/java/team4/packagetrackingapp/TrackPackage.java
@@ -1,6 +1,8 @@
 package team4.packagetrackingapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -8,11 +10,23 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.zxing.integration.IntentIntegrator;
 import com.google.zxing.integration.IntentResult;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Objects;
 
 public class TrackPackage extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     public static final int packageId=0;
@@ -72,10 +86,64 @@ public class TrackPackage extends AppCompatActivity implements AdapterView.OnIte
 
         }
         else {
-            Intent intent = new Intent(this, TrackResultActivity.class);
-            intent.putExtra("packageID", contentTxt.getText().toString());
-            startActivity(intent);
+            SharedPreferences sharedPreferences = getSharedPreferences(LoginActivity.MyPREFERENCES,
+                    Context.MODE_PRIVATE);
+            String username = sharedPreferences.getString("username", null);
+
+            SharedPreferences sP= getSharedPreferences(MainActivity.HOST_Config, Context.MODE_PRIVATE);
+
+            String hostIP= sP.getString("HOST_IP",null);
+            String hostPort= sP.getString("HOST_PORT",null);
+            final String packageID= contentTxt.getText().toString();
+            String URL = "http://"+hostIP+":"+hostPort+"/app/track/"+packageID+"/"+username;
+            System.out.println(URL);
+
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+            JsonObjectRequest objectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    URL,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            JSONObject response_jsonObj = null;
+                            String code = null, msg = null;
+                            Log.e("Rest Response", response.toString());
+
+                            try {
+                                response_jsonObj = response.getJSONObject("Response");
+                                code = response_jsonObj.getString("error_code");
+                            } catch(org.json.JSONException e) {
+                                Log.e("json exception", "thrown");
+                            }
+
+                            if(Objects.equals(code, "000")) {
+                                Intent intent = new Intent(TrackPackage.this, TrackResultActivity.class);
+                                intent.putExtra("packageID", packageID);
+                                startActivity(intent);
+                            } else {
+                                try {
+                                    msg = response_jsonObj.getString("message");
+                                } catch(org.json.JSONException e) {
+                                    Log.e("json exception", "thrown");
+                                } catch(java.lang.NullPointerException e) {
+                                    Log.e("NullPointerException", "thrown");
+                                }
+
+                                Toast.makeText(getApplicationContext(), msg,
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("Rest Error", error.toString());
+                        }
+                    });
+
+            requestQueue.add(objectRequest);
         }
     }
-
 }
